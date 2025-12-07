c1c6cbf470a1f439587ac1697029272704ffc73c
paulomatheus <paulomatheus.araujo@hotmail.com>
2025-12-06 21:02:56 -0300

    Initial Commit

---
+++ inatelposcursos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  inatelposcursos
+//
+//  Created by Paulo Matheus on 06/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ inatelposcursos/inatelposcursosApp.swift
@@ -0,0 +1,17 @@
+//
+//  inatelposcursosApp.swift
+//  inatelposcursos
+//
+//  Created by Paulo Matheus on 06/12/25.
+//
+
+import SwiftUI
+
+@main
+struct inatelposcursosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
